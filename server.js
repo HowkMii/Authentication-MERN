81c3e13a6ee5609315b5430bf76c2da37a15923a
howkmii <abdelhakim@univ-tiaret.dz>
2021-08-23 09:53:54 +0100

    config for only dev

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
+
+//Config env
+require('dotenv').config({
+    path:'./config/config.env'
+})
 const app = express()
 
+//config Dv
 const PORT = process.env.PORT 
-
 app.listen(PORT,()=>{
-    console.log('app listening on port ${PORT}');
+    console.log('app listening on port 5000');
 });
