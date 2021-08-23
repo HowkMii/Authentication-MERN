a9b6a4a58f1fc30d0911c877c02db46e96a1af4c
howkmii <abdelhakim@univ-tiaret.dz>
2021-08-23 10:15:55 +0100

    use routes

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ if(process.env.NODE_ENV==='development'){
     //morgan give information about each request
     //Cors its allow to deal with react
 }
+//load all routes
+const authRouter = require('./routes/auth.route')
+//use routes
+app.use('/api',authRouter);
+app.use((req,res,next)=>{
+    res.status(404).json({
+        success:false,
+        message:"Page Not Founded"
+    })
+});
 const PORT = process.env.PORT 
 app.listen(PORT,()=>{
     console.log('app listening on port 5000');
